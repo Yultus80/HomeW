1eea3d8ed80f614ea3a80be57d5797f79740b974
Yultus80 <yultus@yandex.ru>
2023-06-14 21:12:47 +0300

    дз семинар 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 2:
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+/*Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int max = a;
+if (max < b)
+    max = b;
+Console.WriteLine($"максимальное число = " + max);*/
+
+/*задача 4
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+
+/*Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+if (max < b) 
+max = b;
+if (max < c) 
+max = c;
+
+System.Console.WriteLine("Максимальное из этих чисел = " + max);*/
+
+/*Задача 6: 
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+/*Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+int del = a;
+if (del%2 == 0 )
+Console.WriteLine("Число четное");
+if (del%2 == 1 )
+Console.WriteLine("Число нечетное");*/
+
+/*Задача 8: 
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+/*Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int n = 1;
+while (n <= N)
+{
+    if (n % 2 == 0) 
+    Console.Write($"{n} ");
+    n++;
+}*/
+
+
