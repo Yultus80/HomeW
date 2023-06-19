9cc7ce0e6eff0442037f8dea03392caecd887001
Yultus80 <yultus@yandex.ru>
2023-06-19 20:03:10 +0300

    дз 3 сделана

---
+++ hw3.cs
@@ -0,0 +1,72 @@
+/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и 
+проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+/*Console.Write("Введите введите пятизначное число: ");
+string N = Console.ReadLine()!;
+
+if (N.Length == 5)
+{
+    if (N[0] == N[4] && N[1] == N[3])
+    {
+        Console.WriteLine("Да, число палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Нет, число не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("ВВедено не трехзначное число");
+}*/
+
+
+
+
+/*Задача 21
+Напишите программу, которая принимает на вход координаты двух 
+точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+/*Console.Write("Ввести координату x точки А: ");
+int ax = int.Parse(Console.ReadLine()!);
+
+Console.Write("Ввести координату y точки А: ");
+int ay = int.Parse(Console.ReadLine()!);
+
+Console.Write("Ввести координату y точки А: ");
+int az = int.Parse(Console.ReadLine()!);
+
+Console.Write("Ввести координату x точки B: ");
+int bx = int.Parse(Console.ReadLine()!);
+
+Console.Write("Ввести координату y точки B: ");
+int by = int.Parse(Console.ReadLine()!);
+
+Console.Write("Ввести координату y точки B: ");
+int bz = int.Parse(Console.ReadLine()!);
+
+double AB = Math.Sqrt(Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2) + Math.Pow((bz - az), 2));
+Console.Write($"Расстояние AB = {AB:f2}");*/
+
+/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт 
+таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+
+/*Console.Write("Ввести число N: ");
+int num = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= num; i++)
+{
+    int cube = Convert.ToInt32(Math.Pow(i, 3));
+    Console.WriteLine(cube);
+
+}*/
